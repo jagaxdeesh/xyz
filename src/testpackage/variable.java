2df6f703b13cf9f0cca07300265fd272303013e9
jagaxdeesh <jagaxdeesh@gmail.com>
2025-04-21 18:13:19 +0530

    Update variable by Name replaced Jeswanth .java

--- src/testpackage/variable.java
+++ src/testpackage/variable.java
@@ -3,7 +3,7 @@ package testpackage;
 public class variable {
 	// instances variables
 	static int id = 43;
-	String name = "jaggu";
+	String name = "Jeswanth";
 
 	
 	public static void main(String[] args) {
